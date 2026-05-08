d5e1448d4c2761c57e8b5cdb427faa56adc4548d
Ivan <dziamko02@gmail.com>
2026-05-08 20:29:14 +0300

    fix: move @modal to app root for auto-checker

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,8 +32,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
+  modal,
 }: Readonly<{
   children: React.ReactNode;
+  modal: React.ReactNode;
 }>) {
   return (
     <html lang="en">
@@ -42,6 +44,7 @@ export default function RootLayout({
           <AuthProvider>
             <Header />
             {children}
+            {modal}
             <Footer />
           </AuthProvider>
         </TanStackProvider>
